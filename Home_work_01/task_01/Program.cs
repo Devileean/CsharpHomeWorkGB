c4a38fe67c71adcc44b0089a017dc050c8b21d34
AlexseyLoginov <devileean@gmail.com>
2022-11-12 02:02:32 +0300

    task 1 complete

---
+++ Home_work_01/task_01/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача №1. Напишите программу, которая на вход принимает два числа 
+// и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+int num1 = Convert.ToInt16(Console.ReadLine());
+int num2 = Convert.ToInt16(Console.ReadLine());
+int squareRoot = (num2 * num2);
+if (num1 == squareRoot)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+} 
+
+// не работает с 9 и -3
+
+// int num1 = Convert.ToInt16(Console.ReadLine());
+// int num2 = Convert.ToInt16(Console.ReadLine());
+
+// if (Math.Sqrt(num1) == num2)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// } 
