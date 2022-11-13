47eead2bc95a872d463b877414dabaf6a4eee843
AlexseyLoginov <devileean@gmail.com>
2022-11-13 14:27:30 +0300

    task 3 complete

--- Home_work_01/task_03/Program.cs
+++ Home_work_01/task_03/Program.cs
@@ -4,3 +4,20 @@
 // 3 -> Среда 
 // 5 -> Пятница
 
+Console.Write("Введите число: ");
+int num = Convert.ToInt16(Console.ReadLine());
+
+if (num == 1) 
+    Console.WriteLine("Monday");
+else if (num == 2)
+    Console.WriteLine("Tuesday");
+else if (num == 3)
+    Console.WriteLine("Wednesday");
+else if (num == 4)
+    Console.WriteLine("Thursday");
+else if (num == 5)
+    Console.WriteLine("Friday");
+else if (num == 6)
+    Console.WriteLine("Saturday");
+else if (num == 7)
+    Console.WriteLine("Sunday");
