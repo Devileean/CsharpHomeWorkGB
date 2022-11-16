9f26d823d68ad6428765558f9c83fda2bde5a2b3
AlexseyLoginov <devileean@gmail.com>
2022-11-16 22:58:28 +0300

    task 14 complete

--- Home_work_02/task_14/Program.cs
+++ Home_work_02/task_14/Program.cs
@@ -4,3 +4,13 @@
 // 14 -> нет
 // 46 -> нет
 // 161 -> да
+
+Console.Write("Введите число: "); 
+int num = Convert.ToInt16(Console.ReadLine());
+int seven = num % 7;
+int twentythree = num % 23;
+
+if (seven == 0 && twentythree ==0)
+    Console.WriteLine($"{num} -> да");
+else
+     Console.WriteLine($"{num} -> нет");
