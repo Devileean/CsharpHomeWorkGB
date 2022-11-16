ee9d018becd3c88fc8f6c85648adf812e04f6805
AlexseyLoginov <devileean@gmail.com>
2022-11-16 21:13:06 +0300

    task 9 complete

--- Home_work_02/task_09/Program.cs
+++ Home_work_02/task_09/Program.cs
@@ -4,3 +4,14 @@
 // 78 -> 8
 // 12 -> 2
 // 85 -> 8
+// int firstNum = 0;
+// int secondNum = 0;
+int random = new Random().Next(10, 99);
+Console.WriteLine($"Случайное число: {random}");
+int random2 = random;
+int maxNum = 0;
+  
+do maxNum = random % 10 > maxNum ? random % 10 : maxNum;
+while ((random /= 10) != 0);
+ 
+Console.WriteLine($"{random2} -> {maxNum}");
