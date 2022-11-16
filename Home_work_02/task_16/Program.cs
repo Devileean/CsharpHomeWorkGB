409868036c6d8e4688b37ecfb5901601ce5ba2bc
AlexseyLoginov <devileean@gmail.com>
2022-11-16 19:21:54 +0300

    task 16 complete

--- Home_work_02/task_16/Program.cs
+++ Home_work_02/task_16/Program.cs
@@ -5,3 +5,13 @@
 // -4,16 -> да
 // 25, 5 -> да
 // 8, 9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt16(Console.ReadLine());
+
+if (num2 % num1 == 0 || num1 % num2 == 0)
+    Console.WriteLine($"{num1},{num2} -> да");
+else
+    Console.WriteLine($"{num1},{num2} -> нет");
