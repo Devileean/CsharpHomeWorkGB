051b546a8d658653f8891d0777297658281091dd
AlexseyLoginov <devileean@gmail.com>
2022-11-18 20:18:37 +0300

    task 15 complete

--- Home_work_02/task_15/Program.cs
+++ Home_work_02/task_15/Program.cs
@@ -4,3 +4,16 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+  if (dayWeek == 6 || dayWeek == 7)
+  {
+  Console.WriteLine($"{dayWeek} -> выходной");
+  }
+  else if (dayWeek < 1 || dayWeek > 7)
+  {
+    Console.WriteLine($"{dayWeek} -> не относится ко дню недели");
+  }
+  else Console.WriteLine($"{dayWeek} -> не выходной");
