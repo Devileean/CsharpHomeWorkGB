f1ff1266e968cedff2694be06308e89d180456e4
AlexseyLoginov <devileean@gmail.com>
2022-11-20 22:04:10 +0300

    task 20 complete

--- Home_work_03/task_20/Program.cs
+++ Home_work_03/task_20/Program.cs
@@ -12,6 +12,7 @@ Console.Write("x1: ");
 var x1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("y1: ");
 var y1 = Convert.ToDouble(Console.ReadLine());
+
 Console.WriteLine("Введите координаты точки: B");
 Console.Write("x2: ");
 var x2 = Convert.ToDouble(Console.ReadLine());
@@ -20,7 +21,6 @@ var y2 = Convert.ToDouble(Console.ReadLine());
 
 //AB = √(xb - xa)2 + (yb - ya)2
 var distance =(double) Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-//var distanceConsole = Convert.ToDouble("{0: 0.00}", distance);
-//Console.WriteLine(distance);$"A ({x1},{y1}); B ({x2},{y2}) -> {(
+
 Console.Write($"A ({x1},{y1}); B ({x2},{y2}) ->");
 Console.WriteLine("{0: 0.00}", distance); // округляем до двух знаков после запятой
