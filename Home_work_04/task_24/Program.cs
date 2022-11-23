258ada83e6ca4ccc4f090a1cb49815a240f16a88
AlexseyLoginov <devileean@gmail.com>
2022-11-24 02:17:50 +0300

    task 24 complete

---
+++ Home_work_04/task_24/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задача 24: 
+//  Напишите программу, которая принимает на вход число (А) 
+//  и выдаёт сумма чисел от 1 до А.
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSum(int num)// название метода пишем с большой буквы и как правило глагол
+{
+    int sum = 0;
+    for(int i = 0; i <= num; i++)
+    {
+        sum += i; // каждый раз прибавляем к сумме sum = sum + i
+    }
+    return sum;
+}
+
+Console.WriteLine($"{num} -> {GetSum(num)}");
