feab801eb8dd102c103da4b17ca342ed0721dd26
AlexseyLoginov <devileean@gmail.com>
2022-11-24 23:35:46 +0300

    task 26 complete

---
+++ Home_work_04/task_26/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Задача 26: 
+//  Напишите программу, которая принимает на вход число  
+//  и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите любое число: ");
+int anyNum = Convert.ToInt32(Console.ReadLine());
+
+int NumberOfDigits(int someNum)
+{
+   int length = 0;
+   while (someNum > 0)
+    {
+        length++;
+        someNum /= 10;
+    }
+    return length;
+}
+
+Console.WriteLine($"{anyNum} -> {NumberOfDigits(anyNum)}");
