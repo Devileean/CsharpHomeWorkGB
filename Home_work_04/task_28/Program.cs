f2b36058182413110dec0c13faf91d5cbe661210
AlexseyLoginov <devileean@gmail.com>
2022-11-25 00:58:40 +0300

    task 28 complete

---
+++ Home_work_04/task_28/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Задача 28: 
+//  Напишите программу, которая принимает на вход число N 
+//  и выдаёт произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+int GetMultiply(int someNum)
+{
+    int multiply = someNum;
+    for(int i = 1; i < someNum; i++)
+    {
+        multiply *= i; // multiply = multiply * 10;
+    }
+    return multiply;
+}
+
+Console.Write("Введите любое число: ");
+int anyNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{anyNum} -> {GetMultiply(anyNum)}");
