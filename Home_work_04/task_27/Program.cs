3235911c4470a051bc7d75d34f5d447889214b90
AlexseyLoginov <devileean@gmail.com>
2022-11-29 01:40:57 +0300

    task 27 complete

---
+++ Home_work_04/task_27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: 
+// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumOfNumbers(int num)
+{
+    
+    int sum = 0;
+
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+var anyNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{anyNum} -> " + SumOfNumbers(anyNum));
