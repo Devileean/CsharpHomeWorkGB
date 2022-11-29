c0bbd2ec850d7568c7821ef6bd260e94f88f8330
AlexseyLoginov <devileean@gmail.com>
2022-11-30 00:30:40 +0300

    task 34 complete

--- Home_work_05/task_34/Program.cs
+++ Home_work_05/task_34/Program.cs
@@ -3,3 +3,34 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2 
+
+void Task34()
+{
+    int num1 = GetNum();
+    RandomArray(num1);
+}
+
+void RandomArray(int number) // метод создания рандомного массива
+{
+    var randNum = new Random(); //случайное число
+    int[] elementsArray = new int[number];
+    int even = 0; // переменная для чётных
+    for (var i = 0; i < number; i++)
+    {
+        elementsArray[i] = randNum.Next(100, 1000);
+        if (elementsArray[i] % 2 == 0) //вычисляем четные элементы
+        {
+            even++;
+        }
+    }
+    Console.WriteLine($"[{String.Join(", ", elementsArray)}] -> {even}");
+}
+
+int GetNum()// метод для введения числа
+{
+    Console.Write($"Введите колличество элементов массива: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+Task34();
