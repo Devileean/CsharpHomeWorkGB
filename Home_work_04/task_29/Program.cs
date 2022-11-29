30d1d7f2fff82357ec8cf9ce3392251293768be8
AlexseyLoginov <devileean@gmail.com>
2022-11-29 21:25:04 +0300

    task 29 fix and  complete

--- Home_work_04/task_29/Program.cs
+++ Home_work_04/task_29/Program.cs
@@ -2,13 +2,14 @@
 // Напишите программу, которая задаёт массив из 8 элементов 
 // и выводит их на экран.
 
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
 
 
-
-int[] array = massiv(8);    
+int[] array = MakeMassiv(3);    
 Console.WriteLine($"{String.Join(", ", array)} -> [{String.Join(", ", array)}]");
 
-int[] massiv(int size)
+int[] MakeMassiv(int size)
 {
     int[] result = new int[size];
     for (int i = 0 ; i < size ; i++)
@@ -17,3 +18,19 @@ int[] massiv(int size)
     }
     return result;
 }
+
+//Console.WriteLine("Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.");
+
+// void z29()
+// {
+    
+//     var num = new[] { 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 };
+//     var counter = 0;
+//     while (num.Length > counter)
+//     {
+//         Console.Write(num[counter]);
+//         counter++;
+//     }
+
+// }
+// z29();
