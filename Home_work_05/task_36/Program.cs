ba8f8573b9e2d4b88417b494e4bfed2b9466e753
AlexseyLoginov <devileean@gmail.com>
2022-11-30 00:57:50 +0300

    task 36 complete

---
+++ Home_work_05/task_36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void Task36()  
+{
+    int anyNum = GetNumber();
+    RandomNotEven(anyNum);
+}
+
+void RandomNotEven(int number)
+{
+    var randNum = new Random();
+    int[] elementsArray = new int[number];
+    int summ = 0;
+    for (var i = 0; i < number; i++)
+    {
+        elementsArray[i] = randNum.Next(-99,100);
+        if(i%2!=0)
+        { 
+        summ = summ + elementsArray[i];
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Сумма элементов, стоящих на нечётных позициях:");
+    Console.WriteLine($"[{String.Join(", ", elementsArray)}] -> {summ}");
+}
+
+int GetNumber()
+{
+    Console.Write($"Введите колличество элементов массива: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+Task36();
